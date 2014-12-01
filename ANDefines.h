37c84859399470262cad588ca60f9382c88c5b07
Oksana Kovalchuk <oksana@anoda.mobi>
2014-12-01 14:15:01 +0200

    fix system version

--- ANDefines.h
+++ ANDefines.h
@@ -11,11 +11,13 @@
 #define IS_IPHONE_6     ([[UIScreen mainScreen] bounds].size.height == 667.f)
 #define IS_IPHONE_6_PLUS     ([[UIScreen mainScreen] bounds].size.height == 736.f)
 
+#define IS_IPHONE_5_OR_HIGHER ([[UIScreen mainScreen] bounds].size.height > 568.0f)
+
 #define IS_RETINA ([UIScreen mainScreen].scale == 2)
 
 #define IOS7            (7.0 <= SYSTEM_VERSION && SYSTEM_VERSION < 8.0)
-#define IOS8            (7.0 <= SYSTEM_VERSION && SYSTEM_VERSION)
-#define IOS7_OR_HIGHER  (7.0 <= SYSTEM_VERSION)
+#define IOS8            (8.0 => SYSTEM_VERSION)
+#define IOS7_OR_HIGHER  (7.0 => SYSTEM_VERSION)
 
 #pragma mark Callbacks
 
